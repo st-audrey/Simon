693f5028697729118b36083906fe7bbe07096da5
Audrey <st_audrey@outlook.fr>
2020-11-05 16:38:17 +0100

    draw line linking the last two clicked touches

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -1,8 +1,67 @@
 class Game {
 
     constructor() {
+        this.effectTab =[];
         this.initBoard();
         this.clickable = false;
+        
+    }
+
+    lineEffect(tab){
+
+        //developer.mozilla.org/fr/docs/Web/API/Element/getBoundingClientRect
+        function getOffset(touche) {
+            const rect = touche.getBoundingClientRect();
+            return {
+            left: Math.round(rect.left),
+            top: Math.round(rect.top),
+            right:rect.right, 
+            bottom: rect.bottom, 
+            width: rect.width, 
+            height:rect.height
+            };
+        }
+
+        //récup coordonnées du centre du premier el du tableau
+        //récup coordonnées du centre du deuxième el du tableau
+        var touche0 = tab[0];
+        var touche1 = tab[1];
+
+        var yCenterTouche0 = (getOffset(touche0).top + (getOffset(touche0).height/2));
+        var xCenterTouche0 = (getOffset(touche0).left + (getOffset(touche0).width/2));
+        var yCenterTouche1 = (getOffset(touche1).top + (getOffset(touche1).height/2));
+        var xCenterTouche1 = (getOffset(touche1).left + (getOffset(touche1).width/2));
+
+        console.log(yCenterTouche0);
+        console.log(xCenterTouche0);
+        console.log(yCenterTouche1);
+        console.log(xCenterTouche1);
+        
+
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        ctx.canvas.width  = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+
+        var lineargradient = ctx.createLinearGradient(xCenterTouche0, yCenterTouche0, xCenterTouche1, yCenterTouche1);
+        lineargradient.addColorStop(0, 'red');
+        lineargradient.addColorStop(1, 'blue');  
+        ctx.strokeStyle = lineargradient; 
+
+        ctx.beginPath();
+        ctx.moveTo(xCenterTouche0, yCenterTouche0);
+        ctx.lineTo(xCenterTouche1, yCenterTouche1);
+        ctx.stroke();
+
+    }
+
+    playGame(){
+        let effectTab = this.effectTab;
+        if(effectTab.length >= 2){  
+            var touche = this.effectTab[0];          
+            this.lineEffect(this.effectTab);
+            this.effectTab.shift();          
+        }
     }
 
     initBoard() {
@@ -11,5 +70,9 @@ class Game {
             let touche = new Touche(i,this);           
             container.appendChild(touche);
         }
+        
     }
+
+
+   
 }
--- js/classes/Touche.js
+++ js/classes/Touche.js
@@ -3,17 +3,32 @@ class Touche extends HTMLElement {
     constructor(i, game) {
         super();     
         this.game = game;
-        this.createTouche(i);             
+        this.createTouche(i);
+        this.initListeners();         
+    }
+
+    initListeners() {
+        this.onclick = function () {
+           // if (this.game.clickable) {
+                // var allTouches = document.querySelectorAll(".touche");
+                // for (let i = 0; i < allTouches.length; i++) {
+                //     allTouches[i].classList.remove("click-anime");
+                // }      
+                //this.isPlayed();
+                this.game.effectTab.push(this);
+                console.log(this.game.effectTab);
+                this.game.playGame();
+                //this.game.compareMelodies(this.game.userMelody); 
+           // }
+        }
     }
 
     createTouche(i) {
         var touche = document.createElement("IMG");
         touche.classList.add('touche-img');
         touche.setAttribute('id', 'touche'+i.toString());
-        touche.setAttribute("src", "images/" + i.toString() + ".png");
-        
+        touche.setAttribute("src", "images/" + i.toString() + ".png");        
         this.setAttribute("style", "--i:" + i.toString());
-
         this.appendChild(touche);  
     }
 }
