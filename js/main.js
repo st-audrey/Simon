4ae76132afd3f1bc5b9e1ddc61d5958791f1519a
Audrey <st_audrey@outlook.fr>
2020-11-14 17:53:53 +0100

    add visual of the original version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-var start = document.getElementById("start");
-start.addEventListener("click", function () {
-    start.classList.add("start-fade");
-    new Game(3,0);
-});
+// var start = document.getElementById("start");
+// start.addEventListener("click", function () {
+//     start.classList.add("start-fade");
+//     new Game(3,0);
+// });
+
+var btnOnOff = document.getElementById("btn-on-off");
+btnStart = document.getElementById("btn-start");
+
+btnStart.onclick = function(){ 
+    if (btnOnOff.checked == true){
+        new Game(3,0);
+    }   
+} 
